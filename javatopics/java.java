2d8653558a987a8063aca237cdac4a599ed08aaa
jaimini patel <jaimini.patel029@gmail.com>
2023-02-14 22:32:34 +0000

    NewPractice

---
+++ javatopics/java.java
@@ -0,0 +1,9 @@
+package javatopics;
+
+public class java {
+    public static void main(String[] args) {
+
+        System.out.println("java");
+        
+    }
+}
---
+++ manualtopics/manual.java
@@ -0,0 +1,9 @@
+package manualtopics;
+
+public class manual {
+
+    public static void main(String[] args)
+    {
+        System.out.println("manual");
+    }
+}
